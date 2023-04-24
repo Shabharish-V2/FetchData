0ef5775ed3a9d0f1b51ca40ea5393bdd5faaac6a
Shabharish <111118584+Shabharish-V2@users.noreply.github.com>
2023-04-24 16:59:28 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/data">Data</router-link>
+    <router-link to="/userProfile">userProfile</router-link>
   </nav>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,8 @@ import UserProfile from '@/components/UserProfile.vue'
 const routes: Array<RouteRecordRaw> = [
 
   {
-    path: '/data',
-    name: 'data',
+    path: '/userProfile',
+    name: 'userProfile',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
